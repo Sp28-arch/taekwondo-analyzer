78c15c26704fe9d168c88580a4ae60e9a7ba0911
janfirs <shrey28@my.yorku.ca>
2026-04-16 20:26:43 -0400

    Removed Grayscale: Needed for motion detection not pose detection.

--- video/video_capture.py
+++ video/video_capture.py
@@ -1,53 +1,41 @@
 import cv2
 
 def start_camera():
-   
-   #open webcam
-   
-    cam=cv2.VideoCapture(0)
-
-
+    
+    # open webcam
+    cam = cv2.VideoCapture(0)
 
     # check if the webcam is opened correctly
     if not cam.isOpened():
-      print("Cannot open camera")
-      exit()
-   
+        print("Cannot open camera")
+        exit()
 
-   #Display the video feed
+    # get first frame (previous frame)
+    ret, prev_frame = cam.read()
+    if not ret:
+        print("Failed to read from camera")
+        exit()
+
+
+    # start video loop
     while True:
         ret, frame = cam.read()
-       
 
-       #If no frame is captured, break the loop
         if not ret:
-           break
-
-        
-           #convert the frame to grayscale
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-
-
-
-
-        #Display the frames
-        cv2.imshow('Webcam', gray)
-
-        #Exit the webcam feed when 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-           
             break
 
 
+       
+        # show the frames
+        cv2.imshow('Webcam', frame)
+        
 
-    #Release the webcam and close all windows
+      
+
+        # exit when 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # cleanup
     cam.release()
     cv2.destroyAllWindows()
-
-
-
-        
-        
-        
-       
